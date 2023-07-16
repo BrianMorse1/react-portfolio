7c8994bbd94299685b60dd66f6bc7158dba367e2
Brian Morse <brian.p.morse@gmail.com>
2023-07-16 12:39:40 -0700

    got navbar displaying and added asset photos. Began work on projects page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import ProjectsPage from './pages/Projects';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { Container, Link } from '@chakra-ui/react'
-// import { Link as ReactLink } from "react-router-dom"
+import { Container, Flex, Link, Icon } from '@chakra-ui/react';
+import {  BrowserRouter as Router, 
+    NavLinkLink as RouterLink } from 'react-router-dom';
+
 
 
 function Navbar() {
     return (
         <Container maxW='full'>
-            <Link href='/home'>
+            <Link as={RouterLink} to='/home' p={2}>
             Home
             </Link>
-            <Link href='/about'>
+            <Link as={RouterLink} to='/about' p={2}>
             About
             </Link>
-            <Link href='/projects'>
+            <Link as={RouterLink} to='/projects' p={2}>
             Projects
             </Link>
-            <Link href='/contact'>
+            <Link as={RouterLink} to='/contact' p={2}>
             Contact
+            </Link>
+            <Link as={RouterLink} to='/resume' p={2}>
+            Resume
             </Link>
         </Container>
 )}
